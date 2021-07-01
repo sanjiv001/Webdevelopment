bb8ade264fb6ed0e7188a77801547facc5eae1d7
sanjiv <ssanjiv0001@gmail.com>
2021-07-01 10:16:39 +0545

    ssecond commit

---
+++ javascript.js
@@ -0,0 +1,8 @@
+
+function myfunction(){
+    console.log("Button clicked");
+    window.alert("Button clicked alert");
+    document.write("Button clicked from javascript");
+    document.getElementById("Para1").innerHTML="paragraph from javascript";
+
+}
